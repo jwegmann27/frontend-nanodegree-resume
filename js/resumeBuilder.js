1d9265d7aa1c51d983e50ebb7da636669949517e
jwegmann27 <josephwe27@gmail.com>
2016-01-11 13:32:36 -0500

    Updated Objects to meet specs

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -12,8 +12,37 @@ var bio = {
 	},
 	"location": "Hicksville, NY",
 	"skills": ["cleaning dishes", "HTML", "CSS", "Being a baller"],
-	"image": "images/jwclean.jpg"
+	"biopic": "images/jwclean.jpg"
 };
+
+bio.display = function(){
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+	$("#header").prepend(formattedRole);
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	$("#header").prepend(formattedName);
+	var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+	$("#topContacts").append(formattedMobile);
+	var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+	$("#topContacts").append(formattedEmail);
+	var formattedGit = HTMLgithub.replace("%data%", bio.contacts.git);
+	$("#topContacts").append(formattedGit);
+	var formattedLinked = HTMLtwitter.replace("%data%", bio.contacts.linked);
+	$("#topContacts").append(formattedLinked);
+	var formattedLocation = HTMLlocation.replace("%data%", bio.location);
+	$("#topContacts").append(formattedLocation);
+	var formattedBioPic = HTMLbioPic.replace("%data%",bio.biopic);
+	$("#header").append(formattedBioPic);
+	var formattedWelcomeMSG = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+	$("#header").append(formattedWelcomeMSG);
+
+	$("#header").append(HTMLskillsStart);
+	for(var i =0; i < bio.skills.length; i++){
+	var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+	$("#skills").append(formattedSkill);
+	}//end of for loop
+} //end of bio function
+bio.display();
+
 
 var work = {
 	"jobs": [
@@ -40,70 +69,7 @@ var work = {
 		}
 	]
 };
-
-var education = {
-	"schools": [{
-		"name": "University at Buffalo",
-		"location": "Buffalo, NY, USA",
-		"degree": "Bachelors of Science",
-		"major": "Electrical Engineering",
-		"GPA": "3.37/4.0",
-		"dates": "August 2011 to May 2015"
-	}, {
-		"name": "Ecole Nationale Supérieure de l'Electronique et de ses Applications",
-		"location": "Cergy, France",
-		"degree": "FAME Exchange Program",
-		"major": "Electrical Engineering",
-		"dates": "01/20/14 - 06/15/14"
-	}],
-	"onlineCourse": [{
-		"title": "Front End Web Development",
-		"school": "Udacity.com",
-		"dates": "12/01/15",
-		"url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
-	}]
-};
-
-var projects = {
-	"projects" : [{
-		"title": "Project 1: Online Portfolio",
-		"dates": "11/15/15 - 12/15/15",
-		"description": " Created a responsive web page to act as a platform for future work to be displayed.",
-		"url": "https://github.com/jwegmann27/P1_Potrfolio.git"
-	}]
-};// end of obj 
-
-//Header
-function headerbio(){
-	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-	$("#header").prepend(formattedRole);
-	var formattedName = HTMLheaderName.replace("%data%", bio.name);
-	$("#header").prepend(formattedName);
-	var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
-	$("#topContacts").append(formattedMobile);
-	var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
-	$("#topContacts").append(formattedEmail);
-	var formattedGit = HTMLgithub.replace("%data%", bio.contacts.git);
-	$("#topContacts").append(formattedGit);
-	var formattedLinked = HTMLtwitter.replace("%data%", bio.contacts.linked);
-	$("#topContacts").append(formattedLinked);
-	var formattedLocation = HTMLlocation.replace("%data%", bio.location);
-	$("#topContacts").append(formattedLocation);
-	var formattedBioPic = HTMLbioPic.replace("%data%",bio.image);
-	$("#header").append(formattedBioPic);
-	var formattedWelcomeMSG = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-	$("#header").append(formattedWelcomeMSG);
-
-	$("#header").append(HTMLskillsStart);
-	for(var i =0; i < bio.skills.length; i++){
-	var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
-	$("#skills").append(formattedSkill);
-	}
-
-} //end of bio function
-headerbio();
-
-function displaywork(){
+work.display = function(){
 
 for(job in work.jobs){ //cycles all jobs in the work object 
 	$("#workExperience").append(HTMLworkStart); //The # followed by a title references the html div
@@ -121,8 +87,71 @@ for(job in work.jobs){ //cycles all jobs in the work object
 	$(".work-entry:last").append(formattedWorkDescription);
 }//end of for/in
 }//end of displayWork
+work.display();
 
-displaywork();
+
+var education = {
+	"schools": [{
+		"name": "University at Buffalo",
+		"location": "Buffalo, NY, USA",
+		"degree": "Bachelors of Science",
+		"major": "Electrical Engineering",
+		"GPA": "3.37/4.0",
+		"dates": "August 2011 to May 2015",
+		"url": "http://www.buffalo.edu/"
+	}, {
+		"name": "Ecole Nationale Supérieure de l'Electronique et de ses Applications",
+		"location": "Cergy, France",
+		"degree": "FAME Exchange Program",
+		"major": "Electrical Engineering",
+		"dates": "01/20/14 - 06/15/14",
+		"url": "http://www.ensea.fr/"
+	}],
+	"onlineCourse": [{
+		"title": "Front End Web Development Nanodegree",
+		"school": "Udacity",
+		"dates": "12/01/15 - Present ",
+		"url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+	}]
+};
+
+education.display = function(){
+	$("#education").append(HTMLschoolStart);
+	for (school in education.schools){
+		var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+		$(".education-entry:last").append(formattedSchoolName);
+		var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+		$(".education-entry:last").append(formattedSchoolLocation);
+		var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+		$(".education-entry:last").append(formattedSchoolDegree);
+		var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+		$(".education-entry:last").append(formattedSchoolDates);
+		var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].major);
+		$(".education-entry:last").append(formattedSchoolMajor);
+	}
+	$(".education-entry:last").append(HTMLonlineClasses);
+	for (onlinecourse in education.onlineCourse){
+		var formattedOnlineTitle = HTMLonlineTitle.replace("%data%",education.onlineCourse[onlinecourse].title);
+		$(".education-entry:last").append(formattedOnlineTitle);
+		var formattedOnlineSchool = HTMLonlineSchool.replace("%data%",education.onlineCourse[onlinecourse].school);
+		$(".education-entry:last").append(formattedOnlineSchool);
+		var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourse[onlinecourse].dates);
+		$(".education-entry:last").append(formattedOnlineDates);
+		var formattedOnlineURL = HTMLonlineURL.replace("%data%",education.onlineCourse[onlinecourse].url);
+		$(".education-entry:last").append(formattedOnlineURL);
+	}
+}
+education.display();
+
+var projects = {
+	"projects" : [{
+		"title": "Project 1: Online Portfolio",
+		"dates": "11/15/15 - 12/15/15",
+		"description": " Created a responsive web page to act as a platform for future work to be displayed.",
+		"url": "https://github.com/jwegmann27/P1_Potrfolio.git",
+		"images": ["https://placehold.it/350x150","https://placehold.it/350x150"]
+	}]
+};// end of obj 
 
 projects.display = function(){
 	for (project in projects.projects){
@@ -134,9 +163,12 @@ projects.display = function(){
 		$(".project-entry:last").append(formattedProjectDates);
 		var formattedProjectDescription = HTMLprojectDates.replace("%data%",projects.projects[project].description);
 		$(".project-entry:last").append(formattedProjectDescription);
+		for (var i=0;i < projects.projects[project].images.length;i++){
+			var formattedProjectImages = HTMLprojectImage.replace("%data%",projects.projects[project].images[i]);
+			$(".project-entry:last").append(formattedProjectImages);
+		}
 	}//end of for
 }//end of func
-
 projects.display();
 
 function inName(name) {
